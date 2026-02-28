94ebde5c29e1ec98f5d6d4b4beea194c8544d014
Josué Macedo <josuesampaiomacedo@outlook.pt>
2026-02-28 22:18:22 +0000

    First Launch

---
+++ app.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+ function iniciarAcao() {
+     alert("Aviso lido e aceite. A preparar o processo de Jailbreak...");
+        }
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
